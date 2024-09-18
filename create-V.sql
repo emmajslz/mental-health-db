03fea4268dd18c7c5f52e61dc4c602c838673407
vblante36 <vblante@yahoo.com>
2024-09-18 11:53:00 -0700

    Create Statments- Victoria

---
+++ create-V.sql
@@ -0,0 +1,26 @@
+CREATE TABLE metric(
+    metric_id INT,
+    country_metric VARCHAR(100),
+    type_of_value VARCHAR(100),
+    source VARCHAR(100)
+)
+
+CREATE TABLE social_economical_metrics(
+    soc_eco_id INT,
+    year INT,
+    gdp_per_capita INT,
+    life_expectancy FLOAT,
+    population INT,
+    suicude_total_death FLOAT,
+    daily_income FLOAT,
+    internet_users FLOAT,
+    median_age FLOAT,
+    FOREIGN KEY (country_id) REFERENCES country(country_id), 
+    FOREIGN KEY (metric_id) REFERENCES metric(metric_id)
+)
+
+CREATE TABLE disorder(
+    disorder_id INT,
+    name VARCHAR(100),
+    symptoms TEXT
+)
