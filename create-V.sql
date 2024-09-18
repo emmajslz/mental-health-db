d4d28e82cf77e0cbd1b1a6ffc18d998de178ffc5
vblante36 <vblante@yahoo.com>
2024-09-18 16:28:19 -0700

    Updated create-V.sql

--- create-V.sql
+++ create-V.sql
@@ -1,26 +1,20 @@
-CREATE TABLE metric(
+CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.metric(
     id INT,
     country_metric VARCHAR(100),
     type_of_value VARCHAR(100),
     source VARCHAR(100)
-)
+);
 
-CREATE TABLE social_economical_metrics(
+CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.social_economical_metrics(
     id INT,
     year INT,
-    gdp_per_capita INT,
-    life_expectancy FLOAT,
-    population INT,
-    suicude_total_death FLOAT,
-    daily_income FLOAT,
-    internet_users FLOAT,
-    median_age FLOAT,
+    value FLOAT,
     FOREIGN KEY (country_id) REFERENCES country(id), 
     FOREIGN KEY (metric_id) REFERENCES metric(id)
-)
+);
 
-CREATE TABLE disorder(
+CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.disorder(
     id INT,
     name VARCHAR(100),
     symptoms TEXT
-) 
+); 
