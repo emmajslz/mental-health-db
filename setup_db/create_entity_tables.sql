bb2016f183567c258d1dd757f8544d38c8e3a777
Emma Juan Salazar <emmajuanslz@gmail.com>
2024-09-18 17:11:14 -0700

    correcting our create statements (FK constraints)

--- setup_db/create_entity_tables.sql
+++ setup_db/create_entity_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.Country(
-    id INT PRIMARY KEY, 
+    id VARCHAR(50) PRIMARY KEY, 
     name VARCHAR(500),
     continent VARCHAR(500)
 ); 
@@ -11,14 +11,14 @@ CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.Indicator(
 ); 
 
 CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.metric(
-    id INT,
+    id INT PRIMARY KEY,
     country_metric VARCHAR(100),
     type_of_value VARCHAR(100),
     source VARCHAR(100)
 );
 
 CREATE TABLE MENTAL_HEALTH_HUB.PUBLIC.disorder(
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(100),
     symptoms TEXT
 ); 
